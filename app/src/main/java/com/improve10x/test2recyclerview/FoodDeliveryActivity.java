0bda661e0a4c141dffc0e7b056d92fe508d9dbed
SIVAKUMAR <siva.kumar@Improve10x.com>
2023-03-14 17:21:23 +0530

    added Food items data

--- app/src/main/java/com/improve10x/test2recyclerview/FoodDeliveryActivity.java
+++ app/src/main/java/com/improve10x/test2recyclerview/FoodDeliveryActivity.java
@@ -1,15 +1,56 @@
 package com.improve10x.test2recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class FoodDeliveryActivity extends AppCompatActivity {
+    public Food_items[] food_items;
+    RecyclerView foodItemsRv;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_delivery);
+        initViews();
+        addData();
+
+    }
+
+    private void addData() {
+        food_items = new Food_items[4];
+        Food_items items = new Food_items();
+        items.deliveryStatus = "DELIVERY ON TUESDAY 01";
+        items.userName = "B's Balkans H...";
+        items.location = "ARABIC,INTERNATIONAL";
+        items.DeliveryTime = "USUALLY REPLIES ";
+        items.rating = "4.8";
+        items.minGreen = "12 Min";
+        food_items[0] = items;
+
+        Food_items items1 = new Food_items();
+        items1.deliveryStatus = "DELIVERY ON MONDAY 31";
+        items1.userName = "Syriana";
+        items1.location = "SYRIAN ARABIC";
+        items1.DeliveryTime = "USUALLY ARPLES WITHIN ";
+        items1.rating = "4.3";
+        items1.minGreen = "23 Min";
+        food_items[1] = items1;
+
+        Food_items items2 = new Food_items();
+        items2.deliveryStatus = "DELIVERY ON TUESDAY 01";
+        items2.userName = "B's Balkans H...";
+        items2.location = "ARABIC INTERNATIONAL";
+        items2.DeliveryTime = "USUALLY REPLES WITHIN 12min";
+        items2.rating = "4.8";
+        items2.minGreen = "12 Min";
+        food_items[2] = items2;
+
+    }
+
+    private void initViews() {
+        foodItemsRv = findViewById(R.id.food_item_rv);
     }
 }
---
+++ app/src/main/java/com/improve10x/test2recyclerview/Food_items.java
@@ -0,0 +1,11 @@
+package com.improve10x.test2recyclerview;
+
+public class Food_items {
+    public String deliveryStatus;
+    public String userName;
+    public String location;
+    public String DeliveryTime;
+    public String rating;
+    public String minGreen;
+
+}
