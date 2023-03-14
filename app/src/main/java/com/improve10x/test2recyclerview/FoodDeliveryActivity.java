2a4612aef36993fd2b53b8d96f2ef8a189f1ad20
SIVAKUMAR <siva.kumar@Improve10x.com>
2023-03-14 16:20:23 +0530

    Doing Food items HML

---
+++ app/src/main/java/com/improve10x/test2recyclerview/FoodDeliveryActivity.java
@@ -0,0 +1,15 @@
+package com.improve10x.test2recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodDeliveryActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_delivery);
+    }
+}
